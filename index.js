1eabd363e62bb9f535075cf6d8cb0bf9ccc7dc39
[Lawrencmagnat] <[chukwulawrencep@gmail.com]>
2022-11-22 15:42:29 +0100

    my Inital commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const musicBe = () => {
+    let singer = Davido;
+    let songBe = Jowo;
+    If(music === Jowo);{
+        let songBe = Buga;
+        console.log(songBe);
+
+    }
+    console.log(songBe);
+
+
+
+};
+musicBe(Davido);
