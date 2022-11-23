81d7bfb365591f7aa1e0311fd380b4f4903f02eb
[Lawrencmagnat] <[chukwulawrencep@gmail.com]>
2022-11-23 17:45:39 +0100

    Day 1 of Task

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const musicBe = () => {
+/*const musicBe = () => {
     let singer = Davido;
     let songBe = Jowo;
     If(music === Jowo);{
@@ -11,4 +11,26 @@ const musicBe = () => {
 
 
 };
-musicBe(Davido);
+musicBe(Davido);*/
+
+const articles = ['The proffessor of science says that:','If the student Should study hard in the forth coming exam','They will all pass', 'so i have to study hard'];
+
+const studentResponse = articles[0];
+console.log(articles[3]);
+console.log(articles[4]);
+console.log(articles[1]);
+
+//pratice 2
+const fruitThatIsGood =['Mangoes', 'Paw-paw', 'Oranges', 'Watermelon'];
+fruitThatIsGood[10] =  'kiwi';
+console.log(fruitThatIsGood);
+
+
+let countries = ['Spain', 'England', 'Saudi Arabia', 'Ghana', 'South Africa',];
+countries[3] = 'France';
+countries['Ghana'];
+console.log(countries);
+
+console.log(countries);
+
+
